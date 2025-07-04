641a481e08c5cc43e48414a6342745d047e0f8ba
Otavio-Henrique <otaviohenriquedeoliveiravicen@gmail.com>
2025-07-04 17:24:29 -0300

    roleta russa aba

--- python/sorte.py
+++ python/sorte.py
@@ -12,27 +12,42 @@ def sortear():
     numSorteado = random.randint(1, opcao)
     #print("O número sorteado é: ", numSorteado)
 
-    try:
-        escolha = int(input(f"Escolha um número entre 1 e {opcao}: "))
-        if escolha < 1 or escolha > opcao:
-            raise ValueError("Número fora de intervalo! ")
-    except ValueError as e:
-        print(f"entrada invalida: {e} tente de novo!")
-        sortear()
+    # try:
+    #     escolha = int(input(f"Escolha um número entre 1 e {opcao}: "))
+    #     if escolha < 1 or escolha > opcao:
+    #         raise ValueError("Número fora de intervalo! ")
+        
+    # except ValueError as e:
+    #     print(f"entrada invalida: {e} tente de novo!")
+    #     sortear()
+    def verificarEscolha(escolha):
+        if escolha == numSorteado:
+            print(" Bye Bye word, seu pc será desligado!👻 ")
+            time.sleep(5)
+            if sys.platform == "win32":
+                os.system("shutdown /s /t 1")
+            elif sys.platform == 'linux' or sys.platform == 'linux2':
+                os.system("shutdown now")
+            elif sys.platform == "darwin":
+                os.system("shutduwn -h now")
+        
+        else:
+            print("Você está seguro, por enquanto! ")
+    janela = tk.Toplevel()   
+    janela.title("Escolha um número entre 1 e 6") 
+    tk.Label(janela, text="Escolha um número entre 1 e 6").pack(pady=10)
 
-    if escolha == numSorteado:
-        print(" Bye Bye word, seu pc será desligado!👻 ")
-        time.sleep(5)
-        if sys.platform == "win32":
-            os.system("shutdown /s /t 1")
-        elif sys.platform == 'linux' or sys.platform == 'linux2':
-            os.system("shutdown now")
-        elif sys.platform == "darwin":
-            os.system("shutduwn -h now")
-    
-    else:
-        print("Você está seguro, por enquanto! ")
+    for i in range(1,6):
+        tk.Button(janela, text=str(i), command=lambda i=i: [janela.destroy(), verificarEscolha(i)]).pack(pady=5)
 
 
-sortear()
-            
+root = tk.Tk()
+root.title("Jogo do evento aleatório")
+tk.Label(root, text="Bem-vindo ao Jogo de Evento Aleatório!", font=("Arial", 20)).pack(pady=15)
+tk.Button(root, text="Iniciar Jogo", width=20, command=sortear).pack(pady=10) 
+tk.Button(root, text="Ver regras", width=20, command=sortear).pack(pady=10) 
+tk.Button(root, text="Sair", width=20, command=sortear).pack(pady=10) 
+
+root.mainloop()
+
+
