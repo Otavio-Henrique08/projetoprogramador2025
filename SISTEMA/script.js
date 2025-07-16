1d414838501a403cec2590d79c81025964ac714a
Otavio-Henrique <otaviohenriquedeoliveiravicen@gmail.com>
2025-07-16 17:30:37 -0300

    TELA DE LOGIN

--- SISTEMA/script.js
+++ SISTEMA/script.js
@@ -1,3 +1,18 @@
-$(document).ready( function () {
-    $('#tabela').DataTable();
-} );
+var table = new DataTable('#tabela', {
+    language: {
+        url: 'https://cdn.datatables.net/plug-ins/2.3.2/i18n/pt-BR.json',
+    },
+});
+
+function visualizar(){
+  var senha = $('#senha');
+  var icone = $('#olho');
+
+  if(senha.attr('type') === 'password'){
+    senha.attr('type', 'text');
+    icone.removeClass('fa-eye').addClass('fa-eye-slash');
+  }else{
+    senha.attr('type', 'password');
+    icone.removeClass('fa-eye-slash').addClass('fa-eye');
+  }
+}
